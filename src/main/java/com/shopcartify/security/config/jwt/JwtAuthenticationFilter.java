5b89293c50fb2ec38c6bddbdd8b899970d5b1cda
LordNedStark <osisiogubenjamin1@gmail.com>
2023-10-13 11:17:33 +0100

    working on the security deployed endpoint

--- src/main/java/com/shopcartify/security/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/shopcartify/security/config/jwt/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,6 +19,7 @@ import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
@@ -31,6 +33,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String authHeader = request.getHeader("Authorization");
         final String jwtToken;
         final String userEmail;
+
+        log.info("path:: {}", request.getMethod());
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")
                 || request.getServletPath().equals("/api/v1/auth/register")) {
