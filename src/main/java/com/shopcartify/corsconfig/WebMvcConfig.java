20be47d3c4c92ee746d9a8cb76948ee0dd789365
LordNedStark <osisiogubenjamin1@gmail.com>
2023-10-13 09:59:46 +0100

    made database configurations in application properties,application dev and application prod

--- src/main/java/com/shopcartify/corsconfig/WebMvcConfig.java
+++ src/main/java/com/shopcartify/corsconfig/WebMvcConfig.java
@@ -11,7 +11,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("shopcartifybackend-production.up.railway.app")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
