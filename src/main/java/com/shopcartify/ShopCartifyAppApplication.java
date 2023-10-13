b829b545d87d3bbe91999226b5832e783cc72014
LordNedStark <osisiogubenjamin1@gmail.com>
2023-10-13 11:12:24 +0100

    working on the security deployed endpoint

--- src/main/java/com/shopcartify/ShopCartifyAppApplication.java
+++ src/main/java/com/shopcartify/ShopCartifyAppApplication.java
@@ -2,9 +2,12 @@ package com.shopcartify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -15,4 +18,16 @@ public class ShopCartifyAppApplication {
 		SpringApplication.run(ShopCartifyAppApplication.class, args);
 	}
 
+
+
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*");
+			}
+		};
+	}
 }
--- src/main/java/com/shopcartify/corsconfig/WebMvcConfig.java
+++ src/main/java/com/shopcartify/corsconfig/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.shopcartify.corsconfig;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,6 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
+
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
